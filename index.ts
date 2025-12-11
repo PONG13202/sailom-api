3fca402f72d58a48aa4ddb362dd6148a68356b15
PONG13202 <khunnongmai17915@gmail.com>
2025-12-11 20:44:50 +0700

    final

--- index.ts
+++ index.ts
@@ -30,6 +30,7 @@ app.use(
   })
 );
 
+app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const extraUploadDir = process.env.UPLOAD_DIR || "uploads";
